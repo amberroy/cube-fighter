be78f44e13f8a7545a996d947a75616782e26f2f
Amber Roy <amber.roy@oculus.com>
2019-02-24 21:55:58 -0800

    Added start sound.

--- README.md
+++ README.md
@@ -1,8 +1,9 @@
 ## Cube Fighter - A WebVR Game
-Your mission: destroy all the cubes, armed with nothing but your mighty gaze cursor.
+Your mission: Destroy all the cubes, armed with nothing but your mighty gaze cursor. Currently only one level, but position of cubes is randomly generated if you want to replay.
 
-Currently only one level, but position of cubes is randomly generated if you want to replay. AFAIK this was the World's First WebVR Game built for Gear VR in 2016, now upgraded to run on Oculus Go, just visit https://cubefighter.com in Oculus Browser.
+Cube Fighter was the world's first [WebVR](http://webvr.info/) game built for Gear VR in 2016, now upgraded to run on Oculus Go. Play online at:
+http://CubeFighter.com
 
 ![Cube Fighter screenshot](screenshot.jpg)
 
-Attributions: Based on [a-invaders](https://github.com/dmarcos/a-invaders) by dmarcos and Three.js [webvr_cubes.html](https://github.com/mrdoob/three.js/blob/r75/examples/webvr_cubes.html) by mrdoob. Sound effects from Freesound: [164103](https://www.freesound.org/people/BMacZero/sounds/164103/) [269198](https://www.freesound.org/people/mickleness/sounds/269198/).
+Attributions: Based on [a-invaders](http://github.com/dmarcos/a-invaders) by dmarcos and Three.js [webvr_cubes.html](http://github.com/mrdoob/three.js/blob/r75/examples/webvr_cubes.html) by mrdoob. Sound effects from Freesound: [164103](http://www.freesound.org/people/BMacZero/sounds/164103/) [269198](http://www.freesound.org/people/mickleness/sounds/269198/) [243020](http://www.freesound.org/people/plasterbrain/sounds/243020).
--- libs/explode.js
+++ libs/explode.js
@@ -1,10 +1,10 @@
 // Based on explode.js component for A-Frame https://github.com/dmarcos/a-invaders
-Explode = function(scene, object3D) {
+EXPLODE = function(scene, object3D) {
 
-  if ( object3D instanceof THREE.Group ) {
+  if (object3D instanceof THREE.Group) {
     console.log('converting', object3D);
-    if ( object3D.children && object3D.children.length === 1 &&
-        object3D.children[0] instanceof THREE.Mesh ) {
+    if (object3D.children && object3D.children.length === 1 &&
+        object3D.children[0] instanceof THREE.Mesh) {
       object3D = object3D.children[0];
     } else {
       console.warn('Cannot explode THREE.Group', object3D);
@@ -13,85 +13,86 @@ Explode = function(scene, object3D) {
 
   }
 
-  if ( object3D.geometry instanceof THREE.BufferGeometry ) {
+  if (object3D.geometry instanceof THREE.BufferGeometry) {
     console.log('converting bufferGeometry...', object3D);
-    var geometry = new THREE.Geometry().fromBufferGeometry( object3D.geometry );
+    var geometry = new THREE.Geometry().fromBufferGeometry(object3D.geometry);
     object3D.geometry = geometry;
     console.log('done');
   }
 
-  if ( object3D.geometry instanceof THREE.BufferGeometry ) {
+  if (object3D.geometry instanceof THREE.BufferGeometry) {
     console.warn('Must convert BufferGeometry to Geometry before exploding', object3D);
     return;
   }
 
-    var duration = 8000;
 
-    // explode geometry into objects
-    var pieces = explode(object3D.geometry, object3D.material);
+    // Explode geometry into objects, i.e. sides of the cube.
+    var pieces = explodeGeometry(object3D.geometry, object3D.material);
+    object3D.add(pieces);
 
+    // Hide colored sides of the cube, leaving only the white edges. Must be
+    // done after exploding the pieces, else they will be invisible too.
     object3D.material.visible = false;
 
-    // animate objects
-    for ( var i = 0; i < pieces.children.length; i ++ ) {
+    // Animate the sides to create the explosion effect.
+    var duration = 3000;
+    for (var i = 0; i < pieces.children.length; i ++) {
 
-      var object = pieces.children[ i ];
+      var object = pieces.children[i];
 
       object.geometry.computeFaceNormals();
       var normal = object.geometry.faces[0].normal.clone();
       var targetPosition = object.position.clone().add(normal.multiplyScalar(300));
-      new TWEEN.Tween( object.position )
-        .to( targetPosition, duration )
-        .onComplete( deleteBox )
+      new TWEEN.Tween(object.position)
+        .to(targetPosition, duration)
+        .onComplete(deleteBox)
         .start();
 
       object.material.opacity = 0;
-      new TWEEN.Tween( object.material )
-        .to( { opacity: 1 }, duration )
+      new TWEEN.Tween(object.material)
+        .to({opacity: 1}, duration)
         .start();
 
       var rotation = 2 * Math.PI;
       var targetRotation = { x: rotation, y: rotation, z:rotation };
-      new TWEEN.Tween( object.rotation )
-        .to( targetRotation, duration )
+      new TWEEN.Tween(object.rotation)
+        .to(targetRotation, duration)
         .start();
 
     }
 
     function deleteBox() {
-      object3D.remove( pieces )
-      scene.remove( object3D );
+      object3D.remove(pieces)
+      scene.remove(object3D);
     }
 
-    object3D.add(pieces);
-
-    function explode( geometry, material ) {
+    function explodeGeometry(geometry, material) {
       var pieces = new THREE.Group();
       var material = material.clone();
       material.side = THREE.DoubleSide;
 
       for (var i = 0; i < geometry.faces.length; i ++) {
-        var face = geometry.faces[ i ];
+        var face = geometry.faces[i];
 
-        var vertexA = geometry.vertices[ face.a ].clone();
-        var vertexB = geometry.vertices[ face.b ].clone();
-        var vertexC = geometry.vertices[ face.c ].clone();
+        var vertexA = geometry.vertices[face.a].clone();
+        var vertexB = geometry.vertices[face.b].clone();
+        var vertexC = geometry.vertices[face.c].clone();
 
         var geometry2 = new THREE.Geometry();
-        geometry2.vertices.push( vertexA, vertexB, vertexC );
-        geometry2.faces.push( new THREE.Face3( 0, 1, 2 ) );
+        geometry2.vertices.push(vertexA, vertexB, vertexC);
+        geometry2.faces.push(new THREE.Face3(0, 1, 2));
 
-        var mesh = new THREE.Mesh( geometry2, material );
-        mesh.position.sub( geometry2.center() );
-        pieces.add( mesh );
+        var mesh = new THREE.Mesh(geometry2, material);
+        mesh.position.sub(geometry2.center());
+        pieces.add(mesh);
       }
       //sort the pieces
-      pieces.children.sort( function ( a, b ) {
+      pieces.children.sort(function (a, b) {
         return a.position.z - b.position.z;
         //return a.position.x - b.position.x;     // sort x
         //return b.position.y - a.position.y;   // sort y
-      } );
-      pieces.rotation.set( 0, 0, 0 )
+      });
+      pieces.rotation.set(0, 0, 0)
       return pieces;
     }
 
